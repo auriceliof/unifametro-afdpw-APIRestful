34f2f6ed779b6beea12c23cd2a8c16833af83fc6
auriceliof <auricelio.suporte@hotmail.com>
2024-03-30 14:03:56 -0300

    Created StudentResources and the Endpoint findAll

--- backend/src/main/java/com/unifametro/apirestful/entities/Student.java
+++ backend/src/main/java/com/unifametro/apirestful/entities/Student.java
@@ -2,6 +2,7 @@ package com.unifametro.apirestful.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 public class Student implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -10,6 +11,81 @@ public class Student implements Serializable {
 	private String name;
 	private String cpf;
 	private Instant bihtDate;
-	private Double income;	
+	private Double income;
+	
+	public Student(Long id, String name, String cpf, Instant bihtDate, Double income) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.bihtDate = bihtDate;
+		this.income = income;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Instant getBihtDate() {
+		return bihtDate;
+	}
+
+	public void setBihtDate(Instant bihtDate) {
+		this.bihtDate = bihtDate;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(id, other.id);
+	}	
 }
+
+
+
+
+
+
+
+
+
+
